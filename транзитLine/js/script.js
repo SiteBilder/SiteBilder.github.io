11358ea416ff1fc7597a1e99f6725780171d6b0e
SiteBilder <61997949+SiteBilder@users.noreply.github.com>
2020-04-18 04:18:10 +0300

    Add files via upload

--- транзитLine/js/script.js
+++ транзитLine/js/script.js
@@ -348,7 +348,7 @@ function spoiler() {
 let autopark = $('title').html();
 
 if(autopark == 'Автопарк' || autopark == 'Услуги' ){
-	$('.header').css('background', 'url("img/slider.png") center / cover')
+	$('.header').css('background', 'url("../img/slider.jpg") center / cover')
 
 	$('header').css('height', '244px');
 }
